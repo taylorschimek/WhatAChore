e7456bdfd0fb6550a9a33480bda9fbe3765836d2
Taylor Schimek <ruof@yahoo.com>
2017-05-12 14:42:56 -0500

    put js modal function in app.js.  about to make login a modal.

--- wac/static/wac/js/app.js
+++ wac/static/wac/js/app.js
@@ -1,31 +1,14 @@
-// $('.card').on( 'click', function( event ) {
-//      let title = $( this ).find( '.card-title' ).text();
-//      let theModal = $( "#myModal" );
-//      console.log('trying');
-//      console.log(title)
-//
-//      if ( title == "Create New Chore" ) {
-//           theModal.find( '.modal-title' ).text( 'Create A New Chore' );
-//           theModal.find( '#title' ).val( '' );
-//           theModal.find('#duration').val( '' );
-//           theModal.find( '#frequency' ).val( '' );
-//
-//      } else {
-//           theModal.find( '.modal-title' ).text( 'Edit:' );
-//
-//           theModal.find( '#title' ).val( title );
-//
-//           let duration = $( this ).find( '.cardDuration' ).text();
-//           theModal.find('#duration').val( duration );
-//
-//           let freq = $( this ).find( '.cardFrequency' ).text();
-//           theModal.find( '#frequency' ).val( freq );
-//
-//
-//      }
-//
-//      theModal.modal('toggle');
-// });
+
+function find_modal(theUrl) {
+    var modal = $('.modal');
+    console.log(modal);
+    $.ajax({
+        url: theUrl,
+        context: document.body
+    }).done(function(response) {
+        modal.html(response);
+    });
+}
 
 
 $(".alert").fadeTo(3500, 0).slideUp(500, function(){
