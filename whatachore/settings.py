2b47599fb88b89ed9e991aa14465a2ff2251b060
Taylor Schimek <ruof@yahoo.com>
2017-07-28 15:28:03 -0500

    changed some settings and updated requirements.txt and runtime.txt

--- whatachore/settings.py
+++ whatachore/settings.py
@@ -96,18 +96,19 @@ WSGI_APPLICATION = 'whatachore.wsgi.application'
 # }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'wac_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'monugget1',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wac_db',
+        'USER': 'postgres',
+        'PASSWORD': 'monugget1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
