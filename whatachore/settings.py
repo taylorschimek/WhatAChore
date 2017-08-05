d30952165153e436cea162932e4c96086d7e4a50
Taylor Schimek <ruof@yahoo.com>
2017-08-04 23:47:08 -0500

    email backend back to temp

--- whatachore/settings.py
+++ whatachore/settings.py
@@ -140,16 +140,16 @@ if BROKER_URL == 'django://':
     INSTALLED_APPS += ('kombu.transport.django',)
 
 # SendGrid Stuff -
-EMAIL_HOST = config(EMAIL_HOST)
-EMAIL_PORT = config(EMAIL_PORT, cast=int)
-EMAIL_HOST_USER = config(EMAIL_HOST_USER)
-EMAIL_HOST_PASSWORD = config(EMAIL_HOST_PASSWORD)
-EMAIL_USE_TLS = config(EMAIL_USE_TLS, cast=bool)
+# EMAIL_HOST = config(EMAIL_HOST)
+# EMAIL_PORT = config(EMAIL_PORT, cast=int)
+# EMAIL_HOST_USER = config(EMAIL_HOST_USER)
+# EMAIL_HOST_PASSWORD = config(EMAIL_HOST_PASSWORD)
+# EMAIL_USE_TLS = config(EMAIL_USE_TLS, cast=bool)
 
 # temp for development
-# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-# EMAIL_FILE_PATH = 'tmp/email-messages/'
-# DEFAULT_FROM_EMAIL = 'noreply@taylorschimek.com'
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = 'tmp/email-messages/'
+DEFAULT_FROM_EMAIL = 'noreply@taylorschimek.com'
 
 TEMPLATED_EMAIL_BACKEND = 'templated_email.backends.vanilla_django'
 
