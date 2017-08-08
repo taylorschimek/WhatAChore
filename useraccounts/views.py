de85966d1d062b5a2a611697b364acdea70a9582
Taylor Schimek <ruof@yahoo.com>
2017-08-08 13:29:22 -0500

    pw_email 3

--- useraccounts/views.py
+++ useraccounts/views.py
@@ -37,8 +37,9 @@ from .models import User
 def my_password_reset(request):
     if request.is_ajax() and request.method == 'POST':
         form = PasswordResetRequestForm(data=request.POST)
+        email = []
         if form.is_valid():
-            email = form.cleaned_data['email']
+            email.append(form.cleaned_data['email'])
             user = User.objects.get(email=email)
             token = default_token_generator.make_token(user)
             pw_email.delay(email, token)
